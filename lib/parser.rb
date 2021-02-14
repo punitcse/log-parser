3729a61be786778dd91c800e4c21e9b36e238763
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-02-14 22:17:54 +0000

    Parse log content

---
+++ lib/parser.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Parse the content to easily read the page and IP adresses
+module Parser
+  class Parser
+    attr_reader :content
+
+    class LogLine < OpenStruct; end
+
+    def initialize(content)
+      @content = content
+    end
+
+    def parse
+      logs = []
+
+      content.each do |line|
+        page_name, ip_address = line.split
+        page =  validate_page_name(page_name)
+        logs << LogLine.new(page: page, ip_address: ip_address)
+      end
+      logs
+    end
+
+    private
+
+    def validate_page_name(name)
+      name.delete_prefix('/').split('/').first
+    end
+  end
+end
