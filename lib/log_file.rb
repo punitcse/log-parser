94229adbe36beb964a9a949a156b463ade36e81b
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-02-14 22:18:21 +0000

    rubocop

--- lib/log_file.rb
+++ lib/log_file.rb
@@ -1,29 +1,38 @@
 # frozen_string_literal: true
 
-# Read and validate log file.
-class LogFile
-  VALID_FORMATS = %w[.log].freeze
+module Parser
+  # Read and validate log file.
+  class LogFile
+    VALID_FORMATS = %w[.log].freeze
 
-  class FileNotFound < ArgumentError; end
+    class FileNotFound < ArgumentError; end
 
-  class FileFormatIsInvalid < ArgumentError; end
-  attr_reader :content
+    class FileFormatIsInvalid < ArgumentError; end
+    attr_reader :content
 
-  def initialize(path)
-    @path = path
-    validate!
-    @content = File.readlines(path)
-  end
+    def initialize(path)
+      @path = path
+      validate!
 
-  private
+      @content = File.readlines(path)
+    end
 
-  attr_reader :path
+    # def read_lines
+    #   File.open(path).each_line do |line|
+    #     yield(line)
+    #   end
+    # end
 
-  def validate!
-    not_found_msg = 'File path does not exists. Please check the file path.'
-    raise FileNotFound, not_found_msg unless File.exist?(path)
+    private
 
-    format_not_allowed_msg = "File Format not allowed. Please try with a file with #{VALID_FORMATS.join} extension"
-    raise FileFormatIsInvalid, format_not_allowed_msg unless VALID_FORMATS.include?(File.extname(path))
+    attr_reader :path
+
+    def validate!
+      not_found_msg = 'File path does not exists. Please check the file path.'
+      raise FileNotFound, not_found_msg unless File.exist?(path)
+
+      format_not_allowed_msg = "File Format not allowed. Please try with a file with #{VALID_FORMATS.join} extension"
+      raise FileFormatIsInvalid, format_not_allowed_msg unless VALID_FORMATS.include?(File.extname(path))
+    end
   end
 end
