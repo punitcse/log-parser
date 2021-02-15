ee4c4ff0cb29da0ca44cf70e4a4e0f7c06c0e9bf
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-02-15 00:21:16 +0000

    pass a file argument

--- bin/parser.rb
+++ bin/parser.rb
@@ -4,6 +4,12 @@ require_relative '../lib/parser/log_file'
 require_relative '../lib/parser/page_counter'
 require_relative '../lib/parser'
 
-content = Parser::LogFile.new('webserver.log').content
+file_path = ARGV[0].strip
+
+content = Parser::LogFile.new(file_path).content
 parsed_logs = Parser::Parser.new(content).parse
-puts Parser::PageCounter.new(parsed_logs).count
+counter = Parser::PageCounter.new(parsed_logs)
+
+puts counter.count
+puts '===========================unique views===================================='
+puts counter.count(unique_ip: true)
